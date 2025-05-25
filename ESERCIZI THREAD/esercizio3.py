ee1add89db2f9a1068b12258c62b5e6a3d7a6a0b
albianchi1 <117525722+albianchi1@users.noreply.github.com>
2025-05-25 14:41:55 +0200

    esercizio 3 iniziato

---
+++ ESERCIZI THREAD/esercizio3.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 25 11:35:52 2025
+
+@author: alber
+"""
+from threading import Thread
+
+
+
+
+
+
+def produzione(name, thread):
+    
+    #funzione della produzione
+    
+    
+    
+    
+    
+    
+def consumo(name, thread):
+    #funzione del consumo
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if name == 'main':
+    
