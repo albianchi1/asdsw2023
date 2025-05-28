a03333e27f9150184a54237341fdaaa5e9020b99
albianchi1 <117525722+albianchi1@users.noreply.github.com>
2025-05-28 11:06:55 +0200

    ho aggiunto file di testo esercizio 3 e sono andato avanti con l'implementazione del codice

--- ESERCIZI THREAD/esercizio3.py
+++ ESERCIZI THREAD/esercizio3.py
@@ -5,15 +5,15 @@ Created on Sun May 25 11:35:52 2025
 @author: alber
 """
 from threading import Thread
+import logging
+import os
 
 
 
 
 
 
-def produzione(name, thread):
-    
-    #funzione della produzione
+def produzione(name, thread1):
     
     #vedo se il thread di produzione ha superato una certa soglia
     
@@ -21,16 +21,19 @@ def produzione(name, thread):
     
     
     
-def consumo(name, thread):
     
-    #funzione del consumo
+def consumo(name, thread2):
     
     #vederese è presente almeno un prodotto da prelevare in magazzino
     
+
     
     
+def magazzino(thread1, thread2):
     
-    
+    #funzione del magazzino
+
+
     
     
 if name == 'main':
